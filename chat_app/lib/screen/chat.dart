fe8a2bfea71a317277bce59fa6cd7db88b340b48
Shreyansh Srivastava <shreyanshsri19@gmail.com>
2024-03-04 09:50:11 +0530

    First Commit

---
+++ chat_app/lib/screen/chat.dart
@@ -0,0 +1,56 @@
+import 'package:chat_app/widget/chat_messages.dart';
+import 'package:chat_app/widget/new_messages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+//import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    final token = await fcm.getToken();
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotification();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Chat'),
+        actions: [
+          IconButton(
+            alignment: Alignment.topRight,
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(Icons.exit_to_app),
+            color: Theme.of(context).colorScheme.primary,
+          )
+        ],
+      ),
+      body: const Column(
+        children: [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessages(),
+        ],
+      ),
+    );
+  }
+}
